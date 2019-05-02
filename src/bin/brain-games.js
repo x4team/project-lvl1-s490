d2748273461c37901554982560e26eeebdae3aa2
Dmitry <x4team@gmail.com>
2019-05-02 20:26:31 +0300

    I completed step number 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/bin/node
+import userName from '..';
+
 console.log('Welcome to the Brain Games!');
+const answer = userName('May I have your name? ');
+
+console.log('Hello, ' + answer + '!');
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+
+export default (our_question) => readlineSync.question(our_question);
+
