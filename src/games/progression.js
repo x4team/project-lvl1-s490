70db64c359d95b9a91a8f5956444247b27777658
Dmitry <x4team@gmail.com>
2019-06-13 16:39:40 +0300

    Change brain-progression. All completed last edits

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,34 +1,25 @@
 import makeGame from '..';
 import getRandomInt from '../utils';
 
-const genProgressionData = () => {
-  const gameRules = '\nBrain-progression: What number is missing in the progression?';
-  const genProgression = (array) => {
-    const progressionStep = getRandomInt(8, 20);
-    const progressionLength = 10;
-    const indexLocationNumber = getRandomInt(1, 9);
+const genProgression = (start, length, step, index) => {
+  const arr = [start];
+  for (let counter = length; counter >= 1; counter -= 1) {
+    arr.push(arr[arr.length - 1] + step);
+  }
+  arr.splice(index, 1, '.. ');
+  return arr;
+};
 
-    const progression = [array[0]];
-    const qHeadElements = indexLocationNumber - 1;
-    const qTailElements = progressionLength - indexLocationNumber;
-    const genHead = () => {
-      for (let i = qHeadElements; i >= 1; i -= 1) {
-        progression.unshift(progression[0] - progressionStep);
-      }
-    };
-    const genTail = () => {
-      for (let i = qTailElements; i >= 1; i -= 1) {
-        progression.push(progression[progression.length - 1] + progressionStep);
-      }
-    };
-    genHead();
-    genTail();
-    progression.splice(indexLocationNumber - 1, 1, '.. ');
-    return progression.join(', ');
-  };
-  const number = getRandomInt(1, 100);
-  const answer = `${number}`;
-  const question = genProgression([number]);
+const genProgressionData = () => {
+  const startElement = getRandomInt(1, 100);
+  const lengthProgression = 10;
+  const stepProgression = getRandomInt(1, 20);
+  const indexHidden = getRandomInt(0, 9);
+  const progression = genProgression(startElement, lengthProgression, stepProgression, indexHidden);
+
+  const gameRules = '\nBrain-progression: What number is missing in the progression?';
+  const answer = String(startElement + stepProgression * indexHidden);
+  const question = progression.join(', ');
   return [gameRules, answer, question];
 };
 
